b29db66167646c4cd6fada88dac5d0533189d75e
Henry <36891536+HenryDeLange@users.noreply.github.com>
2020-03-14 13:39:15 +0200

    RELEASE 6.1.2

--- WildLog/src/main/java/wildlog/WildLogApp.java
+++ WildLog/src/main/java/wildlog/WildLogApp.java
@@ -84,9 +84,9 @@ import wildlog.utils.WildLogPaths;
 // Note: Ek kan nie regtig die SwingAppFramework los nie want die progressbar en paar ander goed gebruik dit. Ek sal dan daai goed moet oorskryf...
 public class WildLogApp extends Application {
     private static WildLogApp INSTANCE = null;
-    public static String WILDLOG_VERSION = "6.1.2.BETA";
+    public static String WILDLOG_VERSION = "6.1.2";
     public static Class APPLICATION_CLASS = WildLogApp.class;
-    public static WildLogApplicationTypes WILDLOG_APPLICATION_TYPE = WildLogApplicationTypes.WILDLOG_WEI_ADMIN;
+    public static WildLogApplicationTypes WILDLOG_APPLICATION_TYPE = WildLogApplicationTypes.WILDLOG;
     public static String WILDLOG_USER_NAME = "WildLogUser"; // Default username (when user management is off)
     public static WildLogUserTypes WILDLOG_USER_TYPE = WildLogUserTypes.OWNER; // Default user type (when user management is off)
     public static Logger LOGGER;
